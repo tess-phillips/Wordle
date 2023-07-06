5a25d3db7aa16f999e46afc77124feae9c1f5e2a
tess-phillips <124443397+tess-phillips@users.noreply.github.com>
2023-07-06 13:53:51 +0100

    nothing in script tag now

--- main.js
+++ main.js
@@ -43,3 +43,47 @@ function onSubmit(event){
         const won = createRow(input)
         winLoseCheck(won)}
 }
+
+
+function validateWord(event) {
+    event.preventDefault(); 
+    var input = document.getElementById("inputField").value.toUpperCase();
+    if (wordList.includes(input)) {
+        return input
+    } else {
+        alert("Word is not valid!");
+    }
+}
+
+function createRow(input){
+    const rowTemplate = document.querySelector("#rowTemplate");
+    const domFragment = rowTemplate.content.cloneNode(true);
+    var cells = domFragment.querySelectorAll(".cell");
+    var colouring = guessAssess(randomWord,input)
+    for (var i = 0; i < input.length; i++) {
+        cells[i].textContent = input[i];
+        cells[i].classList.add(colouring[i])
+    }
+    gameBoard.appendChild(domFragment)
+    return allCorrect = colouring.every(element => element === 'correct');
+}
+
+
+function guessAssess(randomWord, input){
+    //output an array of length 5 consisting of 0 (letters of same index match), 1 (letter in input exists in randomword), 2(wrong)
+    var rand = randomWord.split("")
+    var inp = input.split("")
+    return inp.map((x,index)=>{
+        if (rand[index]==x) {return "correct"}
+        else if (rand.includes(x)){return "almost"}
+        else {return "ignore"}
+    })
+}
+
+function getRandomWord(words) {
+    if (!Array.isArray(words) || words.length === 0) {
+        return null; // Return null if the input is not a non-empty array
+    }
+    const randomIndex = Math.floor(Math.random() * words.length);
+    return words[randomIndex];
+}
