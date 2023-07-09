7c025fec9698e9ab3c6a0b2b90f422ab2cf84158
tess-phillips <124443397+tess-phillips@users.noreply.github.com>
2023-07-09 11:04:11 +0100

    getDef js only getting first def now

--- getDefinition.js
+++ getDefinition.js
@@ -1,6 +1,5 @@
 export const getDefinition = async (randomWord) => {
     try {
-        console.log(randomWord)
         const definitionUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${randomWord}`
         const response = await fetch(definitionUrl);
   
@@ -8,10 +7,9 @@ export const getDefinition = async (randomWord) => {
         const data = await response.json();
         // console.log(data."definition")
         const meaning = data[0].meanings;
-        const definition = meaning[0].definitions
-        console.log(definition);
-  
-        return { definition };
+        const definitionArray = meaning[0].definitions
+        const firstDef = definitionArray[0].definition;
+        return firstDef;
       } else {
         throw new Error(response.status);
       }
